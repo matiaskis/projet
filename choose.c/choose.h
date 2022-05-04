f3e90bac5b579122e3ed8e023a68543dd6db0177
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-05-04 22:53:50 +0200

    Add files via upload

---
+++ choose.c/choose.h
@@ -0,0 +1,15 @@
+#ifndef CHOOSE_H
+    #define CHOOSE_H
+    #include <stdio.h>
+   
+#define char *compression = compression
+#define char *decompression = decompression
+
+/*!
+     * @brief this function allows the user to choose the operating mode of the program(compression/decompression).
+     * @param choice operating mode choosed by the user.
+     * @return  integer value used in the main to choose the operating mode.
+     */
+int choose(char **choice);
+
+#endif
