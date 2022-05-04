cefe79e647593cb77e3df7c81495f1e3ac338557
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-05-04 22:19:56 +0200

    intégration du chrono

---
+++ main.c/main.c
@@ -0,0 +1,14 @@
+#include<time.h>
+#include<choose.h>
+
+int main(int argc, char ** argv){
+  clock_t time_start, clock_t time_end;
+  time_start=get_time();
+  int choose=choose(argv);
+ 
+  
+  
+  time_end=get_time();
+  execution_time();
+  return 0;
+}
