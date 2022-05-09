e5874fe3639b367d04c4f6494272a1cbdd0bb666
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-05-09 23:43:14 +0200

    Update choose.h

--- choose.c/choose.h
+++ choose.c/choose.h
@@ -1,15 +1,12 @@
 #ifndef CHOOSE_H
     #define CHOOSE_H
     #include <stdio.h>
-   
-#define char *compression = compression
-#define char *decompression = decompression
-
+  
 /*!
      * @brief this function allows the user to choose the operating mode of the program(compression/decompression).
-     * @param choice operating mode choosed by the user.
+     * @param none.
      * @return  integer value used in the main to choose the operating mode.
      */
-int choose(char **choice);
+int choose();
 
 #endif
