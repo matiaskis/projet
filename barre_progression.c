5472809c592ef6da18e99fb31c98c1478bab96d5
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-05-09 23:48:25 +0200

    Update barre_progression.c

--- barre_progression.c
+++ barre_progression.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
+#include<barre_progression.h>
 
-int test(int a){
+int test(int progression){
     int i;int j;
     printf("\n-------\n");
     printf("|");
-    for(i=0;i<a;i++){
+    for(i=0;i<progression;i++){
             printf("#");
         }
-    for(j=0;j<5-a;j++){
+    for(j=0;j<5-progression;j++){
             printf(" ");
             }
     printf("|");
-     
+    printf("\n-------\n") 
 }
 
