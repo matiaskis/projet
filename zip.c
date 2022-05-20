4113215870b9cd48070ce33e13024b64253ac072
khorz <104204070+khorz@users.noreply.github.com>
2022-05-20 14:01:23 +0200

    changed int with unsigned char

--- zip.c
+++ zip.c
@@ -1,6 +1,6 @@
 
 #include "ppm_lib.h"
-#include "test.h"
+#include "zip.h"
 
 void zip(char **path){
 
@@ -44,30 +44,25 @@ exit(1);
 }
 
 
-
-
-
-
-
 int i, j;
 int last_pixel_value, pixel_value1;
-int counter=0;
+unsigned char counter=0;
 int cache[64]={0};
-int diff_b,diff_r,diff_g,diff_total;
-int block_rgb;
+unsigned char  diff_b,diff_r,diff_g,diff_total;
+unsigned char  block_rgb;
 block_rgb=254;
 last_pixel_value=ppmRead(img_entree,0,0);
-fwrite(&block_rgb,sizeof(int),1,zipped);
+fwrite(&block_rgb,sizeof(unsigned char ),1,zipped);
 
-int red_byte,green_byte,blue_byte;
+unsigned char red_byte,green_byte,blue_byte;
 	red_byte=red(last_pixel_value);
 	green_byte=green(last_pixel_value);
 	blue_byte=blue(last_pixel_value);
 
 
-fwrite(&red_byte,sizeof(int),1,zipped);
-fwrite(&green_byte,sizeof(int),1,zipped);
-fwrite(&blue_byte,sizeof(int),1,zipped);
+fwrite(&red_byte,sizeof(unsigned char ),1,zipped);
+fwrite(&green_byte,sizeof(unsigned char ),1,zipped);
+fwrite(&blue_byte,sizeof(unsigned char ),1,zipped);
 
 
 
@@ -108,7 +103,7 @@ for (i=0;i<height;i++){
 
 		counter=128+64+counter-1; //define bit_fort_block_same 128+64
 		//verifier si la dernier valeur existe deja dans le cache avant de l engistrer dedans
-		fwrite(&counter, sizeof(int),1,zipped);
+		fwrite(&counter, sizeof(unsigned char ),1,zipped);
 		counter=0;
 		}
 	
@@ -121,7 +116,7 @@ for (i=0;i<height;i++){
 		index=(3*red(pixel_value1)+5*green(pixel_value1)+7*blue(pixel_value1))%64;
 
 		if(cache[index]!=0){
-		fwrite(&index,sizeof(int),1,zipped);
+		fwrite(&index,sizeof(unsigned char),1,zipped);
 		}
 
 //block_diff
@@ -134,29 +129,29 @@ diff_b=(blue(pixel_value1)-blue(last_pixel_value)+2)*16;
 
 	
 	diff_total=64+diff_r+diff_b+diff_g; //define bit_fort_block_diff 64
-	fwrite(&diff_total,sizeof(int),1,zipped);
+	fwrite(&diff_total,sizeof(unsigned char),1,zipped);
 }		
 
 
 //block_luma
 else if((green(pixel_value1)-green(last_pixel_value))>=(-32) && (green(pixel_value1)-green(last_pixel_value))<=31 && (red(pixel_value1)-red(last_pixel_value))-(green(pixel_value1)-green(last_pixel_value)) >= (-8) && (red(pixel_value1)-red(last_pixel_value))-(green(pixel_value1)-green(last_pixel_value)) <=7 && (blue(pixel_value1)-blue(last_pixel_value))-(green(pixel_value1)-green(last_pixel_value)) >= (-8) && (blue(pixel_value1)-blue(last_pixel_value))-(green(pixel_value1)-green(last_pixel_value))<=7){
 	diff_g=128+(green(pixel_value1)-green(last_pixel_value))+32;////define bit_fort_block_luma 128
-	fwrite(&diff_g,sizeof(int),1,zipped);
+	fwrite(&diff_g,sizeof(unsigned char),1,zipped);
 	diff_total=((red(pixel_value1)-red(last_pixel_value))-(green(pixel_value1)-green(last_pixel_value))+8)*16+(blue(pixel_value1)-blue(last_pixel_value))-(green(pixel_value1)-green(last_pixel_value))+8;
-	fwrite(&diff_total,sizeof(int),1,zipped);
+	fwrite(&diff_total,sizeof(unsigned char),1,zipped);
 }
 
 //block_rgb
 else{
-	int red_byte,green_byte,blue_byte;
+	
 	red_byte=red(pixel_value1);
 	green_byte=green(pixel_value1);
 	blue_byte=blue(pixel_value1);
 	//define block rgb
-	fwrite(&block_rgb,sizeof(int),1,zipped);
-	fwrite(&red_byte,sizeof(int),1,zipped);
-	fwrite(&green_byte,sizeof(int),1,zipped);
-	fwrite(&blue_byte,sizeof(int),1,zipped);
+	fwrite(&block_rgb,sizeof(unsigned char),1,zipped);
+	fwrite(&red_byte,sizeof(unsigned char),1,zipped);
+	fwrite(&green_byte,sizeof(unsigned char),1,zipped);
+	fwrite(&blue_byte,sizeof(unsigned char),1,zipped);
 }
 	last_pixel_value=pixel_value1;
 	if(cache[index]==0){
@@ -164,7 +159,7 @@ else{
 		}
 }
 }
-printf("zip created");	
+printf("zip created");		
 }
 	
 
