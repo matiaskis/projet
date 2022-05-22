1f9a9aad0a0c06db60410f7ad2976e627cdf8758
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-05-22 23:22:57 +0200

    define bit rgb à tester

--- zip.h
+++ zip.h
@@ -4,6 +4,7 @@
     #define significant_bit_same 192
     #define significant_bit_diff 64
     #define significant_bit_luma 128
+    #define significant_bit_rgb 254
     
 void zip(char **argv);
 #endif
