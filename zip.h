3c5943df32e445a468b818d59d1a3877e6a38564
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-05-22 23:13:35 +0200

    define significant bit

--- zip.h
+++ zip.h
@@ -1,5 +1,9 @@
 #ifndef ZIP_H
     #define ZIP_H
-
+    
+    #define significant_bit_same 192
+    #define significant_bit_diff 64
+    #define significant_bit_luma 128
+    
 void zip(char **argv);
 #endif
