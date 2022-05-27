c55cf6056f7c92616cfa9edb14285724b5495951
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-05-27 19:27:02 +0200

    Update choose.c

--- choose.c/choose.c
+++ choose.c/choose.c
@@ -1,18 +1,13 @@
 #include<choose.h>
 
 int choose(){
- int choice;
- printf("to zip enter 1, to unzip enter 2");
- scanf("%d", &choice);
+    int choice;
  while(choice!=1 && choice!=2){
-  printf("please use a valid argument");
-  printf("to zip enter 1, to unzip enter 2");
- }
+    printf("to zip enter 1, to unzip enter 2");
+    scanf("%d", &choice);
+    }
  if(choice==1){
-
-  return (1);
-}
-else {
-  return (2);    
-}
+    return (1);
+    }
+else return (2);    
 }
