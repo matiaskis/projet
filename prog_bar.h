fe679d2d81e0c5bc26b0156435b3789918839ece
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-05-27 13:34:14 +0200

    Add files via upload

---
+++ prog_bar.h
@@ -0,0 +1,12 @@
+#ifndef PROG_BAR_H
+    #define PROG_BAR_H
+
+    #include<stdio.h>
+    #include<unistd.h>
+
+    /*!
+     * @brief this function print a progression bar
+     * @param none
+     * @return nothing
+     */
+int prog_bar();
