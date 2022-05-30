61983432968ef7e78a6a19720a559c0516b4f6ce
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-05-30 19:15:58 +0200

    Update unzip_v1.c

--- unzip_v1.c
+++ unzip_v1.c
@@ -1,46 +1,46 @@
 #include "unzip.h"
 #include "ppm_lib.h"
 
-void read_param(int *pwidht,int *pheight,int *prange,int *pnbColors,FILE *zip_file){
+void read_param(int *pwidth,int *pheight,int *prange,int *pnbColors,FILE *zip_file){
 
-	unsigned char widht_byte,height_byte,range_byte,nbColors_byte;
+	unsigned char width_byte,height_byte,range_byte,nbColors_byte;
 
-	fread(&widht_byte,sizeof(unsigned char),1,zip_file);
-		if(widht_byte==0){
-	      printf("error in file zip");
+	fread(&width_byte,sizeof(unsigned char),1,zip_file);
+		if(width_byte==0){
+	      printf("error param");
           exit(1);
 	}
-	*pwidht=widht_byte*255;
-	fread(&widht_byte,sizeof(unsigned char),1,zip_file);
-		if(widht_byte==0){
-	      printf("error in file zip");
+	*pwidth=width_byte*255;
+	fread(&width_byte,sizeof(unsigned char),1,zip_file);
+		if(width_byte==0){
+	      printf("error param");
           exit(1);
 	}
-	*pwidht=*pwidht+widht_byte;
+	*pwidth=*pwidth+width_byte;
 
 	fread(&height_byte,sizeof(unsigned char),1,zip_file);
 		if(height_byte==0){
-	      printf("error in file zip");
+	      printf("error param");
           exit(1);
 	}
 	*pheight=height_byte*255;
 	fread(&height_byte,sizeof(unsigned char),1,zip_file);
 		if(height_byte==0){
-	      printf("error in file zip");
+	      printf("error param");
           exit(1);
 	}
 	*pheight=*pheight+height_byte;
 
 	fread(&range_byte,sizeof(unsigned char),1,zip_file);
 		if(range_byte==0){
-	      printf("error in file zip");
+	      printf("error param");
           exit(1);
 	}
 	*prange=range_byte;
 
 	fread(&nbColors_byte,sizeof(unsigned char),1,zip_file);
 	if(nbColors_byte==0){
-	      printf("error in file zip");
+	      printf("error param");
           exit(1);
 	}
 	*pnbColors=nbColors_byte;
@@ -49,22 +49,22 @@ void read_param(int *pwidht,int *pheight,int *prange,int *pnbColors,FILE *zip_fi
 
 
 int read_rgb_block(FILE *zip_file,int j, int i,PPM_IMG* image_sortie){
-	int red,green,blue;
+	unsigned char red,green,blue;
 	int pix;
 
 	fread(&red,sizeof(unsigned char),1,zip_file);
-	if(red==0){
-	      printf("error in file zip");
+	if(red>255 || red<0){
+	      printf("error red_byte");
           exit(1);
 	}
 	fread(&green,sizeof(unsigned char),1,zip_file);
-	if(green==0){
-	      printf("error in file zip");
+	if(green>255 || green<0){
+	      printf("error green byte");
           exit(1);
 	}
 	fread(&blue,sizeof(unsigned char),1,zip_file);
-	if(blue==0){
-	      printf("error in file zip");
+	if(blue>255 || blue<0){
+	      printf("error blue byte");
           exit(1);
 	}
     
@@ -73,9 +73,11 @@ int read_rgb_block(FILE *zip_file,int j, int i,PPM_IMG* image_sortie){
     	return(pix);
 }
     
-int read_same_block(int byte,PPM_IMG* image_sortie,int i,int *pj,int previous_pixel_value){
+int read_same_block(unsigned char byte,PPM_IMG* image_sortie,int i,int *pj,int previous_pixel_value,int width){
 	int counter;
 	counter=byte-192+1;
+
+
 	 for(int k=0;k<counter;k++){
 	                ppmWrite(image_sortie,*pj,i,previous_pixel_value);
 	                *pj=*pj+1;
@@ -83,15 +85,15 @@ int read_same_block(int byte,PPM_IMG* image_sortie,int i,int *pj,int previous_pi
     return(previous_pixel_value);
 }
 
-int read_index_block(int byte,int cache[],int i,int j,PPM_IMG* image_sortie){
-    int index=byte;
+int read_index_block(unsigned char byte,int cache[],int i,int j,PPM_IMG* image_sortie){
+    unsigned char index=byte;
     int pix;
 	pix=cache[index];
 	 ppmWrite(image_sortie,j,i,pix);
 	return(pix);
 }
 
-int read_diff_block(int byte,int previous_pixel_value,int i,int j,PPM_IMG* image_sortie){
+int read_diff_block(unsigned char byte,int previous_pixel_value,int i,int j,PPM_IMG* image_sortie){
     int r,g,b,pix;
     byte=byte-64;
     int diff_red,diff_green,diff_blue;
@@ -107,15 +109,15 @@ int read_diff_block(int byte,int previous_pixel_value,int i,int j,PPM_IMG* image
     return (pix);
 }
     
-int read_luma_block(FILE *zip_file,int byte,int i,int j,PPM_IMG* image_sortie,int previous_pixel_value){
+int read_luma_block(FILE *zip_file,unsigned char byte,int i,int j,PPM_IMG* image_sortie,int previous_pixel_value){
 int diff_green,diff_red,diff_blue;
 byte=byte-128;
 diff_green=byte-32;
 int r,g,b;
 g=green(previous_pixel_value)+diff_green;
 fread(&byte,sizeof(unsigned char),1,zip_file);
-if(byte==0){
-	      printf("error in file zip");
+if(byte<0 || byte>255){
+	      printf("error luma");
           exit(1);
 	}
 diff_red=(byte/16)+diff_green-8;
@@ -162,28 +164,29 @@ exit(1);
 */
 
 	int cache[64]={0};
-	unsigned char pixel_value,previous_pixel_value,index;
+	int pixel_value,previous_pixel_value;
+	unsigned char index;
 
 
-	int widht,height,range,nbColors,rgb_byte;
-	int *pwidht,*pheight,*prange,*pnbColors;
+	int width,height,range,nbColors;
+	unsigned char rgb_byte;
+	int *pwidth,*pheight,*prange,*pnbColors;
 
-	pwidht=&widht;
+	pwidth=&width;
 	pheight=&height;
 	prange=&range;
 	pnbColors=&nbColors;
 
-	read_param(pwidht,pheight,prange,pnbColors,zip_file);
+	read_param(pwidth,pheight,prange,pnbColors,zip_file);
 
 	PPM_IMG *image_sortie;
-	image_sortie=ppmNew(widht,height,range,nbColors);
+	image_sortie=ppmNew(width,height,range,nbColors);
 	
 	
 	
 	fread(&rgb_byte,sizeof(unsigned char),1,zip_file);
-	printf("%d",rgb_byte);
 	if(rgb_byte==0){
-	      printf("error in file zip1");
+	      printf("error rgb_byte non existant");
           exit(1);
 	}
 	
@@ -191,26 +194,27 @@ exit(1);
 	previous_pixel_value=read_rgb_block(zip_file,0,0,image_sortie);
 	}
 	else {
-	    printf("error in file zip2");
+	    printf("error of the rgb_byte value");
 	    exit(1);
 	}
 
 	index=(3*red(previous_pixel_value)+5*green(previous_pixel_value)+7*blue(previous_pixel_value))%64;
 	cache[index]=previous_pixel_value;
 	
-	int byte,j;
+	unsigned char byte;
+	int j;
 	int *pj;
 	pj=&j;
 	for(int i=0;i<height;i++){
-	    for(j=0;j<widht;j++){
+	    for(j=0;j<width;j++){
 	        
 	        if(j==0 && i==0){
 	            j++;
 	        }
 	        
 	        fread(&byte,sizeof(unsigned char),1,zip_file);
-	        	if(byte==0){
-	                printf("error in file zip");
+	        	if(byte<0 || byte>255){
+	                printf("error byte read");
                     exit(1);
             	}
 	        
@@ -220,7 +224,7 @@ exit(1);
 	        }
 	        
 	        else if(byte>192){
-	            pixel_value=read_same_block(byte,image_sortie,i,pj,previous_pixel_value);
+	            pixel_value=read_same_block(byte,image_sortie,i,pj,previous_pixel_value,width);
 	           
 	        }
 	        
