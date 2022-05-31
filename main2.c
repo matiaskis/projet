1354b7c4f4ede0f721d37099ce3cee629e8a792e
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-05-31 20:01:18 +0200

    Add files via upload

---
+++ main2.c
@@ -0,0 +1,23 @@
+#include"time.h"
+#include"choose.h"
+#include"prog_bar.h"
+#include"zip.h"
+#include"unzip.h"
+#include"ppm_lib.h"
+
+int main(){
+PPM_IMG *is;
+	is=ppmNew(64,7,255,3);
+int r=0;int g=0;int b=255;
+int pix=pixel(r,g,b);
+for(int i=0;i<7;i++){
+	for(int j=0;j<63;j++){
+		ppmWrite(is,j,i,pix);
+	}
+b=b-30;
+pix=pixel(r,g,b);
+}
+ppmSave(is,"projet_image/test64");
+ppmClose(is);
+return 0;
+}
