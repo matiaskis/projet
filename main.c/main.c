5216b892ef58cde7f1e14b3dcc905df474efda01
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-06-02 11:32:37 +0200

    Update main.c

--- main.c/main.c
+++ main.c/main.c
@@ -1,18 +1,18 @@
 #include"time.h"
 #include"choose.h"
-#include"prog_bar.h"
 #include"zip.h"
 #include"unzip.h"
 
 int main(int argc, char ** argv){
-  clock_t time_start, clock_t time_end;
+  clock_t time_start,time_end;
   time_start=get_time();
   int choice=choose();
   if(choice==1){
     zip(argv);
   }
-  else unzip(argv);
-  prog_bar();
+  else{
+unzip(argv);
+  }
   time_end=get_time();
   execution_time(time_start,time_end);
   return 0;
