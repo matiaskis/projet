973439090d1ec9cb9484642338327c3f0b422b90
khorz <104204070+khorz@users.noreply.github.com>
2022-06-03 13:58:49 +0200

    Create compar.h

---
+++ compar.h
@@ -0,0 +1,11 @@
+#ifndef COMPAR_H
+    #define COMPAR_H
+    
+	#include <sys/stat.h>
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <fcntl.h>
+	#include <time.h>
+	
+void compar(char **argv, PPM_IMG* image_sortie);
+#endif
