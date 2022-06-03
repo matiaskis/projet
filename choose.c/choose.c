0449d2ea1f79fe7283c8da91b1c73ee194fc2815
khorz <104204070+khorz@users.noreply.github.com>
2022-06-03 12:57:56 +0200

    Update choose.c

--- choose.c/choose.c
+++ choose.c/choose.c
@@ -1,6 +1,6 @@
 #include"choose.h"
 
-void vide_buffer(){
+void vide_buffer(){  //allow to clear the buffer
   while(getc(stdin)!='\n'){
 }
 }
@@ -11,14 +11,14 @@ int choose(){
     char y='y' ,n='n' ;
 
  
- while(choice!=1 && choice!=2 && choice!=3){
+ while(choice!=1 && choice!=2 && choice!=3){//allow to chechk if the user enter the right number
     printf("to zip enter 1, to unzip enter 2, to cancel enter 3 \n");
   
     scanf("%d", &choice);
    vide_buffer();
-    y_n ='a';
+    y_n ='a';// allow to enter on the "while" loop when the user made a mistake in the first choice
 
- while (y_n != 'y' && y_n != 'n' &&  (choice ==1 ||  choice==2  ||choice==3)){
+ while (y_n != 'y' && y_n != 'n' &&  (choice ==1 ||  choice==2  ||choice==3)){ //allow to check if the user enter the right letter
     if(choice==1){
     printf("are you sure that you want to zip\n");
      printf("enter y if yes,enter n if no\n");
@@ -48,16 +48,16 @@ int choose(){
 
     }
 
-    if(y_n==n){
+    if(y_n==n){//allow when the user made a mistake to ask again what he wants
     choice=8;
     }
    }
- if(choice==1){
+ if(choice==1){//the user want zip
     return (1);
     }
 
-else if(choice==2){
+else if(choice==2){//the user want unzip
  return (2);
 }
-else exit(1);
+else exit(1);//the user want cancel
 }
