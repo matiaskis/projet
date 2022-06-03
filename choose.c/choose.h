158b15613742dff25b1241d89e48f2ad40b38093
khorz <104204070+khorz@users.noreply.github.com>
2022-06-03 12:48:22 +0200

    Update choose.h

--- choose.c/choose.h
+++ choose.c/choose.h
@@ -1,12 +1,13 @@
 #ifndef CHOOSE_H
     #define CHOOSE_H
     #include <stdio.h>
-  
+    #include<stdlib.h>
 /*!
      * @brief this function allows the user to choose the operating mode of the program(zip/unzip).
      * @param none.
      * @return  integer value used in the main to choose the operating mode, either 1 or 2.
      */
 int choose();
+void vide_buffer();
 
 #endif
