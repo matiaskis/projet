a9edaad43524951eff8fcc6df4ef88b05c2cf188
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-06-03 13:19:24 +0200

    Update choose.c

--- choose.c/choose.c
+++ choose.c/choose.c
@@ -1,6 +1,6 @@
 #include"choose.h"
 
-void vide_buffer(){  //allow to clear the buffer
+void clear_buffer(){  //clear the buffer
   while(getc(stdin)!='\n'){
 }
 }
@@ -11,20 +11,20 @@ int choose(){
     char y='y' ,n='n' ;
 
  
- while(choice!=1 && choice!=2 && choice!=3){//allow to chechk if the user enter the right number
+ while(choice!=1 && choice!=2 && choice!=3){//check if the user entered the right number
     printf("to zip enter 1, to unzip enter 2, to cancel enter 3 \n");
   
     scanf("%d", &choice);
-   vide_buffer();
-    y_n ='a';// allow to enter on the "while" loop when the user made a mistake in the first choice
+   clear_buffer();
+    y_n ='a';//enter in the "while" loop when the user made a mistake in the first choice
 
- while (y_n != 'y' && y_n != 'n' &&  (choice ==1 ||  choice==2  ||choice==3)){ //allow to check if the user enter the right letter
+ while (y_n != 'y' && y_n != 'n' &&  (choice ==1 ||  choice==2  ||choice==3)){ //check if the user enter the right letter
     if(choice==1){
     printf("are you sure that you want to zip\n");
      printf("enter y if yes,enter n if no\n");
 
     scanf("%c",&y_n);
-       vide_buffer();
+       clear_buffer();
     
    
 
@@ -35,14 +35,14 @@ int choose(){
      printf("enter y if yes,enter n if no\n");
 
     scanf("%c",&y_n); 
-    vide_buffer();
+    clear_buffer();
     }
      else {
     printf("are you sure that you want to cancel\n");
      printf("enter y if yes,enter n if no\n");
   
     scanf("%c",&y_n); 
-         vide_buffer();
+         clear_buffer();
     
     }
 
@@ -52,12 +52,12 @@ int choose(){
     choice=8;
     }
    }
- if(choice==1){//the user want zip
+ if(choice==1){//the user wants zip
     return (1);
     }
 
-else if(choice==2){//the user want unzip
+else if(choice==2){//the user wants unzip
  return (2);
 }
-else exit(1);//the user want cancel
+else exit(1);//the user wants cancel
 }
