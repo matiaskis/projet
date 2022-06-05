ba222ac7b455f380bdcd494a5091f8a53d1a8edb
matiaskis <104195775+matiaskis@users.noreply.github.com>
2022-06-05 16:53:30 +0200

    Update time.c

--- time.c
+++ time.c
@@ -1,13 +1,12 @@
 #include"time.h"
 
 clock_t get_time(){
-  clock_t time;
-    time=clock();
-    return(time);
+  	clock_t time;
+    	time=clock();
+    	return(time);
 }
 
 void execution_time(clock_t time_1,clock_t time_2){
-
 	float execution_time;
 	execution_time=(time_2-time_1)* 1e-6;
 	printf("the operation lasted %fs",execution_time);
